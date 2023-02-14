b8d83c1bd17efe22268bcf10ef7a128610198107
Youtpout <EddyBoughioul@hotmail.fr>
2023-02-14 15:25:03 +0100

    update collection methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Logger, Param, Post, Query, Req, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ImmutableX, Config, UnsignedMintRequest, UnsignedTransferRequest } from '@imtbl/core-sdk';
+import { ImmutableX, Config, UnsignedMintRequest, UnsignedTransferRequest, UpdateCollectionRequest } from '@imtbl/core-sdk';
 import { generateWalletConnection } from './libs/walletConnection';
 import { MintInfo, TransferInfo } from './mint-info';
 import { log } from 'node:console';
@@ -79,4 +79,29 @@ export class AppController {
     }
     return result;
   }
+
+  // @Get('update')
+  // async update(): Promise<any> {
+  //   const walletConnection = await generateWalletConnection('goerli');
+
+  //   const imxClient = new ImmutableX(Config.SANDBOX);
+
+  //   let result: any;
+  //   try {
+  //     const updateParams: UpdateCollectionRequest = {
+  //       name: 'Illumon Hackathon',
+  //       collection_image_url: 'https://imxserver.azurewebsites.net/api/nft/image/',
+  //       description: 'Illumon nft for the illumon game on Imx Hackathon',
+  //       icon_url: 'https://imxserver.azurewebsites.net/api/nft/image/1',
+  //       metadata_api_url: 'https://imxserver.azurewebsites.net/api/nft/'
+  //     };
+
+  //     const mintResponse = imxClient.updateCollection(walletConnection.ethSigner, '0x685576c3a592088ea9ca528b342d05087a64b6e7', updateParams);
+  //     result = mintResponse;
+  //     console.log('mintResponse', JSON.stringify(mintResponse));
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  //   return result;
+  // }
 }
